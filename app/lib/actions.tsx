b5389b1bb650aff77e15c32874426306a1149ace
velopace739 <velopace7@gmail.com>
2024-10-23 05:19:34 +0800

    fix lint

--- app/lib/actions.tsx
+++ app/lib/actions.tsx
@@ -62,6 +62,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
     `;
   } catch (error) {
     // If a database error occurs, return a more specific error.
+    console.log(error);
     return {
       message: 'Database Error: Failed to Create Invoice.',
     };
@@ -100,6 +101,7 @@ export async function updateInvoice(
       WHERE id = ${id}
     `;
   } catch (error) {
+    console.log(error);
     return { message: 'Database Error: Failed to Update Invoice.' };
   }
 
@@ -115,6 +117,7 @@ export async function deleteInvoice(id: string) {
     revalidatePath('/dashboard/invoices');
     return { message: 'Deleted Invoice' };
   } catch (error) {
+    console.log(error);
     return { message: 'Database Error: Failed to Delete Invoice.' };
   }
 }
