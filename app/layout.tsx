7b5542f79983a24c816da913869a98a1efbcd186
velopace739 <velopace7@gmail.com>
2024-10-23 05:34:44 +0800

    update metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,15 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'The simplified version of the financial dashboard',
+  metadataBase: new URL('https://velopace-dashboard.vercel.app'),
+};
 
 export default function RootLayout({
   children,
